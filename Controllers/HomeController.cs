fbc1533d370aa9f45f26a6b30ef86f80e13a5f69
EnriqueEstradaPalomares <jestrada14@ucol.mx>
2022-01-28 13:13:27 -0600

    Login Funcionando

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Kiosko.Models;
+﻿using Kiosko.Data;
+using Kiosko.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,10 +15,12 @@ namespace Kiosko.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly LoginContext _login;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, LoginContext contexto)
         {
             _logger = logger;
+            _login = contexto;
         }
 
         public IActionResult Index()
@@ -31,6 +36,41 @@ namespace Kiosko.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel item)
+        {
+
+
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@Email",item.Email),
+                new SqlParameter("@Pass",item.Pass),
+               /* new SqlParameter("@Usuario", item.Email),*/
+            };
+
+            try
+            {
+                var result = _login.LoginItems.FromSqlRaw<LoginModel>("exec Loggin @Email, @Pass", param).ToList();
+                if (result.Count == 0)
+                {
+                    TempData["msg"] = "Usuario o Contrasena incorrectos, intenete otra vez";
+                }
+                else
+                {
+                    TempData["msg"] = "Bienvenido";
+                    //var res = _KColSoft.KColSoftsItem.FromSqlRaw<KColSoftModel>("exec dbo.RegistroDB @Usuario", param).ToList();
+                }
+
+                return View();
+            }
+
+            catch (Exception ex)
+            {
+                TempData["msg"] = "No encuentro la base de datos";
+                throw;
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Kiosko.Data;
+using Kiosko.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,51 @@ namespace Kiosko.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly LoginContext _login;
+
+        public LoginController(LoginContext contexto)
+        {
+            _login = contexto;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View("Login");
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginModel item)
+        {
+
+
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@Email",item.Email),
+                new SqlParameter("@Pass",item.Pass),
+               /* new SqlParameter("@Usuario", item.Email),*/
+            };
+
+            try
+            {
+                var result = _login.LoginItems.FromSqlRaw<LoginModel>("exec Loggin @Email, @Pass", param).ToList();
+                if (result.Count == 0)
+                {
+                    TempData["msg"] = "Usuario o Contrasena incorrectos, intenete otra vez";
+                }
+                else
+                {
+                    TempData["msg"] = "Bienvenido";
+                    //var res = _KColSoft.KColSoftsItem.FromSqlRaw<KColSoftModel>("exec dbo.RegistroDB @Usuario", param).ToList();
+                }
+
+                return View("Login");
+            }
+
+            catch (Exception ex)
+            {
+                TempData["msg"] = "No encuentro la base de datos";
+                throw;
+            }
+        
         }
     }
 }

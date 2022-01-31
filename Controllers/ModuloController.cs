eb3c93d5a719c94e80ee6518e083d7a663562489
Omar Gomez <og922318@gmail.com>
2022-01-31 10:16:51 -0600

    agregar commit

---
+++ Controllers/ModuloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kiosko.Controllers
+{
+    public class ModuloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
